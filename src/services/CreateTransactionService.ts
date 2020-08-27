2cfc31e26f8897c21a2976b18ce54b6adffe29de
Adriano Oliveira <drprado2@gmail.com>
2020-08-27 09:11:56 -0300

    Implementação das features do desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute(request: Request): Transaction {
+    if (request.type === 'outcome') {
+      const currentBalance = this.transactionsRepository.getBalance();
+      if (currentBalance.total < request.value)
+        throw Error('There is not enouh balance');
+    }
+    return this.transactionsRepository.create({
+      title: request.title,
+      value: request.value,
+      type: request.type,
+    });
   }
 }
 
